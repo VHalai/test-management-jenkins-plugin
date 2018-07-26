a8609891a90e1a9a42ef528f938cfdad134bbc2b
Vitali Halai <vitali_halai@epam.com>
2018-07-26 11:02:52 +0300

    add new branch

---
+++ test-management-jenkins-plugin-master/target/generated-sources/localizer/org/jenkinsci/plugins/Messages.java
@@ -0,0 +1,384 @@
+// CHECKSTYLE:OFF
+
+package org.jenkinsci.plugins;
+
+import org.jvnet.localizer.Localizable;
+import org.jvnet.localizer.ResourceBundleHolder;
+
+
+/**
+ * Generated localization support class.
+ * 
+ */
+@SuppressWarnings({
+    "",
+    "PMD",
+    "all"
+})
+public class Messages {
+
+    /**
+     * The resource bundle reference
+     * 
+     */
+    private final static ResourceBundleHolder holder = ResourceBundleHolder.get(Messages.class);
+
+    /**
+     * Key {@code FormValidation.AuthorizationFailed}: {@code Authorization
+     * failed}.
+     * 
+     * @return
+     *     {@code Authorization failed}
+     */
+    public static String FormValidation_AuthorizationFailed() {
+        return holder.format("FormValidation.AuthorizationFailed");
+    }
+
+    /**
+     * Key {@code FormValidation.AuthorizationFailed}: {@code Authorization
+     * failed}.
+     * 
+     * @return
+     *     {@code Authorization failed}
+     */
+    public static Localizable _FormValidation_AuthorizationFailed() {
+        return new Localizable(holder, "FormValidation.AuthorizationFailed");
+    }
+
+    /**
+     * Key {@code FormValidation.UnknownError}: {@code Unknown error, status
+     * code:}.
+     * 
+     * @return
+     *     {@code Unknown error, status code:}
+     */
+    public static String FormValidation_UnknownError() {
+        return holder.format("FormValidation.UnknownError");
+    }
+
+    /**
+     * Key {@code FormValidation.UnknownError}: {@code Unknown error, status
+     * code:}.
+     * 
+     * @return
+     *     {@code Unknown error, status code:}
+     */
+    public static Localizable _FormValidation_UnknownError() {
+        return new Localizable(holder, "FormValidation.UnknownError");
+    }
+
+    /**
+     * Key {@code FormValidation.EmptyUsername}: {@code The Jira user name
+     * cannot be empty}.
+     * 
+     * @return
+     *     {@code The Jira user name cannot be empty}
+     */
+    public static String FormValidation_EmptyUsername() {
+        return holder.format("FormValidation.EmptyUsername");
+    }
+
+    /**
+     * Key {@code FormValidation.EmptyUsername}: {@code The Jira user name
+     * cannot be empty}.
+     * 
+     * @return
+     *     {@code The Jira user name cannot be empty}
+     */
+    public static Localizable _FormValidation_EmptyUsername() {
+        return new Localizable(holder, "FormValidation.EmptyUsername");
+    }
+
+    /**
+     * Key {@code FormValidation.InvalidUrl}: {@code The Jira URL must start
+     * with http:// or https://}.
+     * 
+     * @return
+     *     {@code The Jira URL must start with http:// or https://}
+     */
+    public static String FormValidation_InvalidUrl() {
+        return holder.format("FormValidation.InvalidUrl");
+    }
+
+    /**
+     * Key {@code FormValidation.InvalidUrl}: {@code The Jira URL must start
+     * with http:// or https://}.
+     * 
+     * @return
+     *     {@code The Jira URL must start with http:// or https://}
+     */
+    public static Localizable _FormValidation_InvalidUrl() {
+        return new Localizable(holder, "FormValidation.InvalidUrl");
+    }
+
+    /**
+     * Key {@code Date.Month}: {@code Month}.
+     * 
+     * @return
+     *     {@code Month}
+     */
+    public static String Date_Month() {
+        return holder.format("Date.Month");
+    }
+
+    /**
+     * Key {@code Date.Month}: {@code Month}.
+     * 
+     * @return
+     *     {@code Month}
+     */
+    public static Localizable _Date_Month() {
+        return new Localizable(holder, "Date.Month");
+    }
+
+    /**
+     * Key {@code Date.Week}: {@code Week}.
+     * 
+     * @return
+     *     {@code Week}
+     */
+    public static String Date_Week() {
+        return holder.format("Date.Week");
+    }
+
+    /**
+     * Key {@code Date.Week}: {@code Week}.
+     * 
+     * @return
+     *     {@code Week}
+     */
+    public static Localizable _Date_Week() {
+        return new Localizable(holder, "Date.Week");
+    }
+
+    /**
+     * Key {@code FormValidation.ServerNoFound}: {@code Incorrect URL, Jira
+     * server not found}.
+     * 
+     * @return
+     *     {@code Incorrect URL, Jira server not found}
+     */
+    public static String FormValidation_ServerNoFound() {
+        return holder.format("FormValidation.ServerNoFound");
+    }
+
+    /**
+     * Key {@code FormValidation.ServerNoFound}: {@code Incorrect URL, Jira
+     * server not found}.
+     * 
+     * @return
+     *     {@code Incorrect URL, Jira server not found}
+     */
+    public static Localizable _FormValidation_ServerNoFound() {
+        return new Localizable(holder, "FormValidation.ServerNoFound");
+    }
+
+    /**
+     * Key {@code Label.Build.Date}: {@code Build date}.
+     * 
+     * @return
+     *     {@code Build date}
+     */
+    public static String Label_Build_Date() {
+        return holder.format("Label.Build.Date");
+    }
+
+    /**
+     * Key {@code Label.Build.Date}: {@code Build date}.
+     * 
+     * @return
+     *     {@code Build date}
+     */
+    public static Localizable _Label_Build_Date() {
+        return new Localizable(holder, "Label.Build.Date");
+    }
+
+    /**
+     * Key {@code Date.Minute}: {@code Minute}.
+     * 
+     * @return
+     *     {@code Minute}
+     */
+    public static String Date_Minute() {
+        return holder.format("Date.Minute");
+    }
+
+    /**
+     * Key {@code Date.Minute}: {@code Minute}.
+     * 
+     * @return
+     *     {@code Minute}
+     */
+    public static Localizable _Date_Minute() {
+        return new Localizable(holder, "Date.Minute");
+    }
+
+    /**
+     * Key {@code ResultsRecorder.DisplayName}: {@code Jira Test Management
+     * Results Updater}.
+     * 
+     * @return
+     *     {@code Jira Test Management Results Updater}
+     */
+    public static String ResultsRecorder_DisplayName() {
+        return holder.format("ResultsRecorder.DisplayName");
+    }
+
+    /**
+     * Key {@code ResultsRecorder.DisplayName}: {@code Jira Test Management
+     * Results Updater}.
+     * 
+     * @return
+     *     {@code Jira Test Management Results Updater}
+     */
+    public static Localizable _ResultsRecorder_DisplayName() {
+        return new Localizable(holder, "ResultsRecorder.DisplayName");
+    }
+
+    /**
+     * Key {@code Date.Hour}: {@code Hour}.
+     * 
+     * @return
+     *     {@code Hour}
+     */
+    public static String Date_Hour() {
+        return holder.format("Date.Hour");
+    }
+
+    /**
+     * Key {@code Date.Hour}: {@code Hour}.
+     * 
+     * @return
+     *     {@code Hour}
+     */
+    public static Localizable _Date_Hour() {
+        return new Localizable(holder, "Date.Hour");
+    }
+
+    /**
+     * Key {@code FormValidation.EmptyPassword}: {@code The Jira user
+     * password cannot be empty}.
+     * 
+     * @return
+     *     {@code The Jira user password cannot be empty}
+     */
+    public static String FormValidation_EmptyPassword() {
+        return holder.format("FormValidation.EmptyPassword");
+    }
+
+    /**
+     * Key {@code FormValidation.EmptyPassword}: {@code The Jira user
+     * password cannot be empty}.
+     * 
+     * @return
+     *     {@code The Jira user password cannot be empty}
+     */
+    public static Localizable _FormValidation_EmptyPassword() {
+        return new Localizable(holder, "FormValidation.EmptyPassword");
+    }
+
+    /**
+     * Key {@code Label.Build.Number}: {@code Build number}.
+     * 
+     * @return
+     *     {@code Build number}
+     */
+    public static String Label_Build_Number() {
+        return holder.format("Label.Build.Number");
+    }
+
+    /**
+     * Key {@code Label.Build.Number}: {@code Build number}.
+     * 
+     * @return
+     *     {@code Build number}
+     */
+    public static Localizable _Label_Build_Number() {
+        return new Localizable(holder, "Label.Build.Number");
+    }
+
+    /**
+     * Key {@code Date.Year}: {@code Year}.
+     * 
+     * @return
+     *     {@code Year}
+     */
+    public static String Date_Year() {
+        return holder.format("Date.Year");
+    }
+
+    /**
+     * Key {@code Date.Year}: {@code Year}.
+     * 
+     * @return
+     *     {@code Year}
+     */
+    public static Localizable _Date_Year() {
+        return new Localizable(holder, "Date.Year");
+    }
+
+    /**
+     * Key {@code Date.Day}: {@code Day}.
+     * 
+     * @return
+     *     {@code Day}
+     */
+    public static String Date_Day() {
+        return holder.format("Date.Day");
+    }
+
+    /**
+     * Key {@code Date.Day}: {@code Day}.
+     * 
+     * @return
+     *     {@code Day}
+     */
+    public static Localizable _Date_Day() {
+        return new Localizable(holder, "Date.Day");
+    }
+
+    /**
+     * Key {@code FormValidation.InternalError}: {@code Internal Server
+     * Error. Please, check your credentials}.
+     * 
+     * @return
+     *     {@code Internal Server Error. Please, check your credentials}
+     */
+    public static String FormValidation_InternalError() {
+        return holder.format("FormValidation.InternalError");
+    }
+
+    /**
+     * Key {@code FormValidation.InternalError}: {@code Internal Server
+     * Error. Please, check your credentials}.
+     * 
+     * @return
+     *     {@code Internal Server Error. Please, check your credentials}
+     */
+    public static Localizable _FormValidation_InternalError() {
+        return new Localizable(holder, "FormValidation.InternalError");
+    }
+
+    /**
+     * Key {@code FormValidation.ConnectionSuccessful}: {@code Connection
+     * successful}.
+     * 
+     * @return
+     *     {@code Connection successful}
+     */
+    public static String FormValidation_ConnectionSuccessful() {
+        return holder.format("FormValidation.ConnectionSuccessful");
+    }
+
+    /**
+     * Key {@code FormValidation.ConnectionSuccessful}: {@code Connection
+     * successful}.
+     * 
+     * @return
+     *     {@code Connection successful}
+     */
+    public static Localizable _FormValidation_ConnectionSuccessful() {
+        return new Localizable(holder, "FormValidation.ConnectionSuccessful");
+    }
+
+}
